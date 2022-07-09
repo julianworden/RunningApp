e4aa7e30afa29c8f74f39ba1c85bb6323b6b4e46
julianworden <julianworden@gmail.com>
2022-07-09 15:23:10 -0700

    Initial Commit

---
+++ RunningApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RunningApp
+//
+//  Created by Julian Worden on 7/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
